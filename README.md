91a485cd85f2881fc15aa43f3aa6804db2dc4eea
Michael Hofer <karras@0x539.ch>
2022-01-09 23:30:20 +0100

    feat: refactor overall pipeline, add pkg signing and repo database
    BREAKING CHANGE: the build script and its options were heavily changed

--- README.md
+++ README.md
@@ -2,21 +2,46 @@
 
 Arch Linux AUR package builds, published via GitHub Releases.
 
-![GitHub](https://github.com/karras/aur-package-builds/workflows/Build%20&%20Publish/badge.svg?branch=main)
+[![Build & Publish](https://github.com/karras/aur-package-builds/actions/workflows/build-publish.yml/badge.svg)](https://github.com/karras/aur-package-builds/actions/workflows/build-publish.yml)
 
 ## Packages
 
-The following packages are covered:
+The following packages are covered (see [package.lst](./package.lst)):
 
 * [greetd](https://aur.archlinux.org/packages/greetd/)
 * [greetd-gtkgreet](https://aur.archlinux.org/packages/greetd-gtkgreet/)
 * [wayfire](https://aur.archlinux.org/packages/wayfire/)
 * [wf-config](https://aur.archlinux.org/packages/wf-config/)
 
-## Builds
+## Usage
 
 The actual package builds can be found in the latest
-[Releases](https://github.com/karras/aur-package-builds/releases).
+[Releases](https://github.com/karras/aur-package-builds/releases). All releases
+also include the required repository database in order to install them directly
+via pacman:
+
+* Import and trust the [package signing key](./builder_public_key.asc):
+  ```sh
+  pacman-key --add builder_public_key.asc
+  pacman-key --lsign-key 25267573FD638312C5EBE4C40C758F9503EDE7AF
+  ```
+
+* Add the repository to `/etc/pacman.conf` (replace `$RELEASE` in the URL with
+  the desired version):
+  ```ini
+  [karras]
+  Server = https://github.com/karras/aur-package-builds/releases/download/$RELEASE
+  ```
+
+* Refresh the local repository databases:
+  ```sh
+  pacman -Sy
+  ```
+
+* Install the required packages (e.g. `wayfire`):
+  ```sh
+  pacman -S wayfire
+  ```
 
 ## License
 
--- build.sh
+++ build.sh
@@ -1,16 +1,45 @@
-#!/bin/sh
+#!/bin/sh -
 #
-# Creates a directory in /tmp, clones the AUR repository into it and finally
-# builds the package. Definitely not gold, lots of room for improvement.
+# Creates a temporary build directory in /tmp and builds all packages in there
+# based on the provided package list file. Also supports signing the packages
+# and creates an appropriate repository database. The articats are finally
+# stored in one build directory.
 #
-# ./build.sh GIT_URL [COMMIT_HASH]
+# Supported environment variables:
+#
+#   PACKAGE_AUTHOR:      Defines who is the packager, set to 'John Doe
+#                        <john@example.com>' if not defined
+#
+#   PACKAGE_BASE_URL:    Base URL where the source repositories are located,
+#                        set to 'https://aur.archlinux.org' if not defined
+#
+#   PACKAGE_CONFIG:      File containing the package list to build, set to
+#                        'packages.lst' if not defined
+#
+#   PACKAGE_DESTINATION: Defines where to store the built packages, set to
+#                        '$HOME/build' if not defined
+#
+#   PACKAGE_GPG_ID:      GPG ID of the private key to use for signing the
+#                        packages, if not set the packages will not be signed
+#
+# USAGE: ./build.sh
 
 set -eo pipefail
 
-# Required tools
-DEPENDENCIES="mktemp git"
+readonly DEPENDENCIES="id git makepkg pacman-key repo-add"
 
-# Test if dependencies are available
+readonly PKG_BASE_URL="${PACKAGE_BASE_URL:-https://aur.archlinux.org}"
+readonly PKG_CFG="${PACKAGE_CONFIG:-packages.lst}"
+
+# Specific options for makepkg and repo-add, see their respective man pages
+export GPGKEY="${PACKAGE_GPG_ID:-}"
+export PKGDEST="${PACKAGE_DESTINATION:-${HOME}/build}"
+export PACKAGER="${PACKAGE_AUTHOR:-John Doe <john@example.com>}"
+
+# Required by makepkg to ensure signature files are stored along the packages
+export SRCPKGDEST="${PKGDEST}"
+
+# Check if all dependencies are available
 for DEPENDENCY in ${DEPENDENCIES}; do
   if [[ ! $(type "${DEPENDENCY}" 2> /dev/null) ]]; then
     echo "Dependency '${DEPENDENCY}' not found in PATH, exiting..."
@@ -18,25 +47,59 @@ for DEPENDENCY in ${DEPENDENCIES}; do
   fi
 done
 
-if [[ -z "${1}" ]]; then
-  echo "No AUR git URL provided as the first parameter, exiting..."
+# Check if we are executed as root which does not work with makepkg
+if [[ "$(id -u)" -eq 0 ]]; then
+  echo "Script must not be executed as root, exiting..."
+  exit 1
+fi
+
+echo $HOME
+# Check if package config file exists
+if [[ ! -f "${PKG_CFG}" ]]; then
+  echo "No file named '${PKG_CFG}' found at script location, exiting..."
   exit 1
 fi
 
 # Create temporary build directory
-BUILD_DIR=$(mktemp --directory --suffix=pkgbuild)
+readonly TMP_BUILD_DIR=$(mktemp --directory --suffix=pkgbuild)
 
-# Clone (AUR) repository
-git clone "${1}" "${BUILD_DIR}"
-
-# Switch to build directory
-cd "${BUILD_DIR}"
-
-# Optionally checkout specific commit (i.e. AUR version pinning)
-if [[ ! -z "${2}" ]]; then
-  echo "Checking out commit '${2}'"
-  git checkout --quiet "${2}"
+# Create package destination directory if required
+echo "All packages will be placed in '${PKGDEST}'"
+if [[ ! -d "${PKGDEST}" ]]; then
+  mkdir "${PKGDEST}"
 fi
 
-# Build package
-makepkg --noconfirm --syncdeps
+# Build all packages
+while read -r PACKAGE; do
+  # Skip all lines starting with a hashtag
+  [[ "${PACKAGE}" =~ ^#.*$ ]] && continue
+
+  echo "Starting build process for package '${PACKAGE}'"
+
+  # Clone source repository
+  git clone "${PKG_BASE_URL}/${PACKAGE}" "${TMP_BUILD_DIR}/${PACKAGE}"
+
+  # Build package
+  cd "${TMP_BUILD_DIR}/${PACKAGE}"
+  if [[ ! -z "${GPGKEY}" ]]; then
+    echo "Package will be built and signed with the GPG key '${GPGKEY}'"
+    makepkg --noconfirm --syncdeps --install --sign
+  else
+    echo "Package will be built without signing it"
+    makepkg --noconfirm --syncdeps --install
+  fi
+done < "${PKG_CFG}"
+
+echo "Finished building all packages, check the '${PKGDEST}' directory"
+
+ls -al "${PKGDEST}"
+
+if [[ ! -z "${GPGKEY}" ]]; then
+  echo "Creating package repository database and sign it with the GPG key '${GPGKEY}'"
+  repo-add --sign "${PKGDEST}/karras.db.tar.xz" ${PKGDEST}/*.zst
+else
+  echo "Creating package repository database without signing it"
+  repo-add "${PKGDEST}/karras.db.tar.xz" ${PKGDEST}/*.zst
+fi
+
+echo "Finished generating repository database, check the '${PKGDEST}' directory"
